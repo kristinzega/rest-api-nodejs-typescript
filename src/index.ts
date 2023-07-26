46e9a722bb7c52a36ee3283ec52fa42b4c0f0f14
Kristin Zega <kristianni.zega08@gmail.com>
2023-07-26 09:22:24 +0700

    Setup ESLint, Prettier, and Husky

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
-import express, { Application, NextFunction, Request, Response } from "express";
+// eslint-disable-next-line @typescript-eslint/consistent-type-imports
+import express, { Application, NextFunction, Request, Response } from 'express'
 
-const app: Application = express();
-const port: Number = 4000;
+const app: Application = express()
+// eslint-disable-next-line @typescript-eslint/ban-types
+const port: Number = 4000
 
-app.use("/health", (req: Request, res: Response, next: NextFunction) => {
-    res.status(200).send({ status: "200" });
-});
+app.use('/health', (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).send({ status: '200' })
+})
 
-app.listen(port, () => console.log(`Server is listening on port ${port}`));
+// eslint-disable-next-line @typescript-eslint/no-confusing-void-expression
+app.listen(port, () => console.log(`Server is listening on port ${port}`))
